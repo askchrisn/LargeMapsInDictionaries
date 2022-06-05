217f8a8498c26daeb8c6e8b5e3a4d0c4a1f894eb
askchrisn <fundasappteam@gmail.com>
2022-06-05 01:59:56 -0400

    More Maps

--- LargePngFramework/Program.cs
+++ LargePngFramework/Program.cs
@@ -12,7 +12,7 @@ namespace LargePngFramework
     internal class Program
     {
         // Configs
-        private const int SIZE = 25;
+        private const int SIZE = 1;
         private const int SIZE_IN_PIXELS = SIZE*600; // 1px = 50cm = 600 pixels = 1m SQFT
         private const string IMAGE_PATH = @"C:\Users\chris\Documents\Projects\LargeMapsInDictionaries\Maps\";
         
